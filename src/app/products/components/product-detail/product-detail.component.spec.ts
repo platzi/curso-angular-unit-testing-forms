b2ef61e1f9c2d41bf0cdf0ec9516b20a4810ad0f
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-01 12:59:43 -0400

    :white_check_mark: Skip detail test

--- src/app/products/components/product-detail/product-detail.component.spec.ts
+++ src/app/products/components/product-detail/product-detail.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDetailComponent } from './product-detail.component';
 
-describe('ProductDetailComponent', () => {
+xdescribe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
 
